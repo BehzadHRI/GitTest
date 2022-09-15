2eceb9b2d451f9e33d64e55ba16e0f3ff613f933
Jeppe <JeppePedersen2706@gmail.com>
2022-09-15 11:02:16 +0200

    Det gode du ved

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
         System.out.println("Hello, World!");
         System.out.println("HEllo Again");
+        System.out.println("Jeppe er så fucking lækker jeg vil ødelægge hans numsehul");
     }
 }
 
